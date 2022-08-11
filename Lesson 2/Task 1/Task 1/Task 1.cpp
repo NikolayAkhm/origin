8310256f8491bd8a8f10ab305ba2dd082b2a2df6
Nikolay Akhmetov <nikolaaarn@gmail.com>
2022-08-11 15:24:03 +0500

    Lesson 2 task 1 solution added.

---
+++ Lesson 2/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+enum class Month
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+int main()
+{
+    bool exit = true;
+    while (exit) 
+    {
+        int num;
+        std::cout << "Enter month number " << "\n";
+        std::cin >> num;
+        std::cout << Month::;
+    }
+    return 0;
+}
