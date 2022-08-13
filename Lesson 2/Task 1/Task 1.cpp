3b202a2bc9578ae58f16c0b16b7881b946754b7c
Nikolay Akhmetov <nikolaaarn@gmail.com>
2022-08-13 09:27:32 +0500

    Lesson 2 task 1 solution add
    Output the name by the number of the month.

--- Lesson 2/Task 1/Task 1.cpp
+++ Lesson 2/Task 1/Task 1.cpp
@@ -1,8 +1,10 @@
 ﻿
 #include <iostream>
+#include <string>
+
 enum class Month
 {
-    January,
+    January = 1,
     February,
     March,
     April,
@@ -17,13 +19,90 @@ enum class Month
 };
 int main()
 {
-    bool exit = true;
-    while (exit) 
-    {
+        bool exit = true;
         int num;
         std::cout << "Enter month number " << "\n";
         std::cin >> num;
-        std::cout 
-    }
-    return 0;
+        while (exit)
+        {
+            switch (num)
+            {
+            case 0:
+                std::cout << "Goodbye";
+                exit = false;
+                break;
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::January) :
+                std::cout << "January\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::February) :
+                std::cout << "February\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::March):
+                std::cout << "March\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::April):
+                std::cout << "April\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::May):
+                std::cout << "May\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::June):
+                std::cout << "June\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::July):
+                std::cout << "July\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::August):
+                std::cout << "August\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::September):
+                std::cout << "September\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::October):
+                std::cout << "October\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::November) :
+                std::cout << "November\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            case static_cast<int>(Month::December) :
+                std::cout << "December\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            default:
+                std::cout << "Error when entering month number\n";
+                std::cout << "Enter month number " << "\n";
+                std::cin >> num;
+                break;
+            }
+  
+        }
+
+
+return 0;
 }
