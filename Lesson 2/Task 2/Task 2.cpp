e764338f83fbc7d5bdbe12b367bfaaa6518904dd
Nikolay Akhmetov <nikolaaarn@gmail.com>
2022-08-13 11:17:46 +0500

    Lesson 2 task 2 solution add
    Create a structure with a bank account.

---
+++ Lesson 2/Task 2/Task 2.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <string>
+
+struct bank_account
+{
+    std::string first_name;
+    int account_number;
+    double money;
+};
+void increment_age(bank_account& b)
+{
+    double num;
+    std::cout << "Enter new balance\n";
+    std::cin >> num;
+    b.money = num;
+}
+
+int main()
+{
+    bank_account first;
+    std::cout << "Enter first_name\n";
+    std::cin >> first.first_name;
+    std::cout << "Enter account number\n";
+    std::cin >> first.account_number;
+    std::cout << "Enter amount of money\n";
+    std::cin >> first.money;
+    increment_age(first);
+
+    std::cout << "Your bank account: " << first.first_name << ", " << first.account_number << ", " << first.money ;
+    return 0;
+}
