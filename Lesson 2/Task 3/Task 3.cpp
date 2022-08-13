3ed3c416351c75a988e624d95e28251c33b923a4
Nikolay Akhmetov <nikolaaarn@gmail.com>
2022-08-13 15:01:34 +0500

    Lesson 2 task 3 solution add
    Create an address structure and a function to display the structure on the screen.

---
+++ Lesson 2/Task 3/Task 3.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <string>
+
+struct address
+{
+    std::string city;
+    std::string street;
+    std::string house_number;
+    std::string apartment_number;
+    std::string index;
+};
+void show_structure(address& a)
+{
+    std::cout << "City: " << a.city << "\n";
+    std::cout << "Street: " << a.street << "\n";
+    std::cout << "House number: " << a.house_number << "\n";
+    std::cout << "Apartment number: " << a.apartment_number << "\n";
+    std::cout << "Index: " << a.index << "\n\n";
+
+}
+
+int main()
+{
+    address Oren = { "Orenberg", "st. Dzerzhinskogo" , "12", "12", "460000" };
+    address Sam = { "Samara", "st. Krasnoarmejskaya" , "12", "12", "443000" };
+    show_structure(Oren);
+    show_structure(Sam);
+        return 0;
+}
